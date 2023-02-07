a6132c6c53fefedb148ad0e50cae0a36c8ea7e3c
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-02-07 20:46:07 +0900

    feat: first init

---
+++ src/main/java/org/shupool/shupoolbackend/ShuPoolBackendApplication.java
@@ -0,0 +1,13 @@
+package org.shupool.shupoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuPoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuPoolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shupool/shupoolbackend/ShuPoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shupool.shupoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuPoolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
