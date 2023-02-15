6d9a05d9e463ff6a806699da12259c44ac373f05
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-02-15 18:44:50 +0900

    feat: User 모델 작성

---
+++ src/main/java/org/shupool/shupoolbackend/domain/user/User.java
@@ -0,0 +1,43 @@
+package org.shupool.shupoolbackend.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String picture;
+
+    @Column
+    private String kakaoId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String picture, String kakaoId, Role role) {
+        this.name = name;
+        this.picture = picture;
+        this.kakaoId = kakaoId;
+        this.role = role;
+    }
+}
