43f046929ebdd0618a4036fad38ecdacf134bb59
Song kyeong seok <skj0922@kookmin.ac.kr>
2023-02-19 14:56:25 +0900

    fix: SecurityConfig 설정 버그 수정

--- src/main/java/org/shupool/shupoolbackend/config/SecurityConfig.java
+++ src/main/java/org/shupool/shupoolbackend/config/SecurityConfig.java
@@ -1,50 +1,44 @@
 package org.shupool.shupoolbackend.config;
 
 import lombok.RequiredArgsConstructor;
-import org.shupool.shupoolbackend.config.jwt.JwtAuthenticationFilter;
 import org.shupool.shupoolbackend.config.jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//            .httpBasic().disable()
-//            .csrf().disable()
-//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            .and()
-//            .authorizeRequests()
-//            .antMatchers("/api/v1/login").permitAll()
-//            .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-//            .anyRequest().authenticated()
-//            .and()
-//            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-        http.httpBasic().disable();
+        http.csrf().disable()
+            .headers().frameOptions().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeHttpRequests()
+            .antMatchers("/swagger-ui/**", "/h2-console/**", "/v3/api-docs/**").permitAll()
+            .antMatchers("/admin").hasRole("ROLE_ADMIN")
+            .anyRequest().authenticated();
+
         http.formLogin().disable();
-        http.authorizeRequests()
-            .antMatchers("/api/v1/login").permitAll()
-            .antMatchers("/swagger-ui.html/**", "/h2-console/**").permitAll()
-//            .anyRequest().authenticated()
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-        http.csrf().disable();
+
+        http.httpBasic().disable();
 
         return http.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
