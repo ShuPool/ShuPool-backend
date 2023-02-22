e5fa6f3b89ed963c4d720339f2541abfedc3a6d8
songks0922 <skj0922@kookmin.ac.kr>
2023-02-22 16:38:13 +0900

    feat: SwaggerConfig authentication 설정 추가

--- src/main/java/org/shupool/shupoolbackend/config/SwaggerConfig.java
+++ src/main/java/org/shupool/shupoolbackend/config/SwaggerConfig.java
@@ -1,19 +1,31 @@
 package org.shupool.shupoolbackend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
-                .title("ShuPool API")
-                .description("ShuPool API")
-                .version("1.0.0");
-        return new OpenAPI().info(info);
+        Info info = new Info().title("ShuPool").version("1.0.0")
+                .description("ShuPool 애플리케이션 API입니다.")
+                .termsOfService("http://swagger.io/terms/");
+
+
+        SecurityScheme basichAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("basic");
+        SecurityRequirement securityItem = new SecurityRequirement().addList("basicAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("basicAuth", basichAuth))
+                .addSecurityItem(securityItem)
+                .info(info);
     }
 }
