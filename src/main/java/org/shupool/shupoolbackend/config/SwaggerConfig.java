6c758f3f1c7a052c9e71af4d93a3bcfb2bda38be
songks0922 <skj0922@kookmin.ac.kr>
2023-02-22 15:45:31 +0900

    feat: swagger 설정

--- src/main/java/org/shupool/shupoolbackend/config/SwaggerConfig.java
+++ src/main/java/org/shupool/shupoolbackend/config/SwaggerConfig.java
@@ -1,5 +1,19 @@
 package org.shupool.shupoolbackend.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("ShuPool API")
+                .description("ShuPool API")
+                .version("1.0.0");
+        return new OpenAPI().info(info);
+    }
 }
